85358d0bea5bc2c17e6be1a3c6a9e31a6ffb81ac
zsheng <zsheng.dev@gmail.com>
2025-03-21 10:36:22 -0400

    fix: resolve remark web-worker usage
    When using remark in web worker, one of the dependent libraries
    does not detect that and uses the browser version `index.dom.js` which
    uses `document`, causing an error.
    
    To fix this, we need to force to always use the web worker version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,53 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import path from 'path';
 import { defineConfig } from 'vite';
 const host = process.env.TAURI_DEV_HOST;
 
+// Find the actual path to the package
+function findPackagePath(packageName: string) {
+  const basePath = path.resolve(__dirname, 'node_modules/.pnpm');
+  const folders = fs.readdirSync(basePath);
+
+  // Look for folders that start with the package name
+  const packageFolder = folders.find((folder) =>
+    folder.startsWith(`${packageName}@`)
+  );
+
+  if (packageFolder) {
+    return path.join(
+      basePath,
+      packageFolder,
+      'node_modules',
+      packageName,
+      'index.js'
+    );
+  }
+
+  return null;
+}
+
 // https://vitejs.dev/config/
-// @ts-expect-error tailwindcss plugin always has this type error
+// @ts-expect-error  tailwindcss plugin always has this type error
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      // the reason this exists is because the when we use remark in a web worker,
+      // it's dependent on this dependency, but the version used is `index.dom.js`,
+      // which references `document` that doesn't exist in web worker
+      // This is a known issue for a while and the workaround is specified here:
+      // https://github.com/orgs/remarkjs/discussions/1083#discussioncomment-4347349
+      'decode-named-character-reference':
+        findPackagePath('decode-named-character-reference') ||
+        'decode-named-character-reference',
     },
+  },
+  worker: {
+    format: 'es',
+    plugins: [],
   },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
