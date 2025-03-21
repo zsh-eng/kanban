3b641a20a98245ebb797fcbf0b8f27111f105593
zsheng <zsheng.dev@gmail.com>
2025-03-21 11:03:04 -0400

    chore: add check text area util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isTextInput(event: KeyboardEvent) {
+  return (
+    event.target instanceof HTMLElement &&
+    (event.target.isContentEditable ||
+      event.target.tagName === 'INPUT' ||
+      event.target.tagName === 'TEXTAREA')
+  );
 }
